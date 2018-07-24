da9d7702878d9c318d1249f5b8cd7894578685d6
raislansilva <raislan@live.com>
2018-07-24 06:58:39 -0500

    concluindo crud sqlite

--- src/pages/edit-product/edit-product.ts
+++ src/pages/edit-product/edit-product.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { ProductProvider } from '../../providers/product/product';
+import { CategoryProvider } from '../../providers/category/category';
+import { Product } from '../../models/Product';
 
 /**
  * Generated class for the EditProductPage page.
@@ -14,12 +17,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edit-product.html',
 })
 export class EditProductPage {
+  model: Product
+  categories: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    private toast: ToastController,
+    private productProvider: ProductProvider,
+    private categoryProvider: CategoryProvider,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+      this.model = new Product
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditProductPage');
-  }
+      if(this.navParams.data.id){
+        this.productProvider.get(this.navParams.data.id)
+        .then((result:any)=>{
+          this.model= result;
+        })
+      }
+
+    }
+
+    ionViewDidLoad(){
+      this.categoryProvider.getAll()
+      .then((result: any[])=> {
+        this.categories =  result;
+      })
+      .catch(()=> {
+        this.toast.create({message:'Erro ao carregar as categorias', duration:3000, position:'botton'}).present();
+      })
+    }
+
+    save(){
+      this.saveProduct()
+        .then(()=>{
+          this.toast.create({message: 'Produto salvo', duration:3000, position:'botton'}).present();
+        })
+        .catch(()=>{
+           this.toast.create({message:'Erro ao salvar', duration:3000,position:'botton'}).present();
+        })
+      
+
+      
+    }
+
+    saveProduct(){
+      if(this.model.id){
+        return this.productProvider.update(this.model);
+      }else{
+        return this.productProvider.insert(this.model);
+
+      }
+    }
+
+ 
 
 }
